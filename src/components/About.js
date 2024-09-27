b09a3267e485fff3a646639bcb1fa75b4669ebde
mohanraj-exe <smohan1029@gmail.com>
2024-09-27 14:11:15 +0530

    mobile_v_styling_fixed

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import '../styles/Aboutus.css';
 function About() {
     return (
         <>
-            <section className='aboutus-section' id='aboutus-section'>
+            <section className='aboutus-section'>
                 <section className='container'>
                     <article>
                         <h2 className='aboutus-title'>Who we are?</h2>
                         <img src="Mario-and-Adrian-A.jpg" alt="restaurant-owners-img" />
-                        <h3>Mario and adrian</h3>
+                        <h3 className='aboutus-owners-title'>Mario and adrian</h3>
                         <p>
                             Mario and Adrian, lifelong friends with a shared love for culinary arts, founded Little Lemon with a simple mission: to bring fresh, vibrant Mediterranean flavors to their community. With roots deeply embedded in Italian and Greek traditions, they wanted to create a space where every dish tells a story of their heritage, combined with modern twists that excite the palate.
                         </p>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,9 +8,9 @@ function Testimonials() {
           <h2 className='testimonials-title'>Testimonials!</h2>
 
           <article className='review-section'>
-            <h3>Username</h3>
+            <h3 className='username'>Username</h3>
             <img src="greek-salad.jpg" alt="user-profile-img" />
-            <h4>Rating</h4>
+            <h4 className='rating'>Rating</h4>
             <p className='review'>
               "The bruschetta was a delightful start to our meal.
               The bread was perfectly toasted, and the tomato topping was fresh and flavorful with just the right amount of garlic and basil.
@@ -19,9 +19,9 @@ function Testimonials() {
           </article>
 
           <article className='review-section'>
-          <h3>Username</h3>
+          <h3 className='username'>Username</h3>
           <img src="greek-salad.jpg" alt="user-profile-img" />
-            <h4>Rating</h4>
+            <h4 className='rating'>Rating</h4>
             <p className='review'>
               "A classic Greek salad done right! The crunchy cucumbers, juicy tomatoes, and briny olives were delicious, and the red onions added a nice kick.
               The feta cheese was generously portioned, and the olive oil dressing tied it all together beautifully."
@@ -29,9 +29,9 @@ function Testimonials() {
           </article>
 
           <article className='review-section'>
-          <h3>Username</h3>
+          <h3 className='username'>Username</h3>
           <img src="lemon-dessert.jpg" alt="user-profile-img" />
-            <h4>Rating</h4>
+            <h4 className='rating'>Rating</h4>
             <p className='review'>
               "This lemon dessert was absolutely divine!
               The tartness of the lemon was balanced perfectly with just the right amount of sweetness.
@@ -40,9 +40,9 @@ function Testimonials() {
           </article>
 
           <article className='review-section'>
-          <h3>Username</h3>
+          <h3 className='username'>Username</h3>
           <img src="greek-salad.jpg" alt="user-profile-img" />
-            <h4>Rating</h4>
+            <h4 className='rating'>Rating</h4>
             <p className='review'>
               "Loved the bruschetta! The combination of ripe tomatoes, fresh basil, and a drizzle of balsamic glaze made for an explosion of flavors.
               The bread was crispy but not too hard, making each bite a pleasure."
